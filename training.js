816f0de74f2b73864f7ded7da1b2641c04fd9e37
Alexander <96749659+Nazeofel@users.noreply.github.com>
2023-07-14 22:30:18 +0200

    Added comments to functions and reworked some of them

--- training.js
+++ training.js
@@ -4,27 +4,28 @@ let trainingSet;
 const container = document.querySelector("#training-container");
 
 function GetArrayOfRangedNumber(mininclusive, maxexclusive) {
-  let r = [];
+  const r = [];
   for (let i = mininclusive; i < maxexclusive; i++) {
     r.push(i);
   }
   return r;
 }
+// Generates the trainingDataSet
 
-function GenerateTraining(dataset, length, errset = [], errPower = 5) {
+/*
+ *
+ * [data, list, symbols, index]
+ *
+ */
+function GenerateTraining(dataset, length) {
   if (dataset.symbol === undefined) {
     return (container.innerHTML =
       "An error occured while fetching the symbols");
   }
   let shuffleList = GetArrayOfRangedNumber(0, dataset.symbol.length);
-  if (errset.length > 0) {
-    for (let i = 0; i < errPower; i++) {
-      shuffleList.push(errset);
-    }
-  }
 
-  let list = [];
-  let symbolContainerList = [];
+  const list = [];
+  const symbolContainerList = [];
 
   for (let i = 0; i < length; i++) {
     let c = Math.floor(Math.random() * shuffleList.length);
@@ -39,65 +40,70 @@ function GenerateTraining(dataset, length, errset = [], errPower = 5) {
     }
   }
 
-  let trainingSet = [dataset, list, symbolContainerList, 0];
+  const trainingSet = [dataset, list, symbolContainerList, 0];
 
   return trainingSet;
 }
-
+// appends trainingSet to the input and add the TrainingListener function as the event
 function StartListening(trainingSet) {
-  document.getElementById("training-input").trainingSet = trainingSet;
-  document.getElementById("training-input").errList = [];
+  document.querySelector("#training-input").trainingSet = trainingSet;
   document
-    .getElementById("training-input")
+    .querySelector("#training-input")
     .addEventListener("input", TrainingListener);
 }
-
-function UpdateCounter() {
-  document.getElementById("correct-counter").innerHTML = `Correct : ${correct}`;
-  document.getElementById("bad-counter").innerHTML = `Bad : ${bad}`;
-}
-
+// is called anytime an input is entered and checks with the help of other functions if it's right or not
 function TrainingListener(evt) {
-  let trainingSet = document.getElementById("training-input").trainingSet;
-  let errList = document.getElementById("training-input").errList;
-  let value = evt.srcElement.value;
-  let currentIndex = trainingSet[1][trainingSet[3]];
-  let symbolList = trainingSet[0].symbol;
-  let romajiList = trainingSet[0].romaji;
+  const trainingSet = document.querySelector("#training-input").trainingSet;
+  const value = evt.srcElement.value;
+  const currentIndex = trainingSet[3];
+  const indexOfCurrentSymbol = trainingSet[2][currentIndex];
+  const symbolList = trainingSet[0].symbol;
+  const romajiList = trainingSet[0].romaji;
 
   if (evt.data === " ") {
-    if (value.slice(0, -1) === romajiList[currentIndex]) {
-      setSymbolCorrectness(trainingSet[2][trainingSet[3]], true);
+    if (value === romajiList[currentIndex]) {
+      setSymbolCorrectness(indexOfCurrentSymbol, true);
       correct++;
     } else {
-      setSymbolCorrectness(trainingSet[2][trainingSet[3]], false);
-      errList.push(currentIndex);
+      setSymbolCorrectness(indexOfCurrentSymbol, false);
       bad++;
     }
     evt.srcElement.value = "";
     trainingSet[3]++;
-
-    document.getElementById("last-symbol").innerHTML = symbolList[currentIndex];
-    document.getElementById("last-transcription").innerHTML =
-      romajiList[currentIndex];
-
+    updateAnswerPanel(symbolList, romajiList, currentIndex);
     UpdateCounter();
   }
 
+  // currentIndex >= trainingSet[2] replace everything and reload new test
   if (trainingSet[3] >= trainingSet[2].length) {
     document
-      .getElementById("training-input")
+      .querySelector("#training-input")
       .removeEventListener("input", TrainingListener);
     clearAndLoadData();
   }
 }
 
+// updates panel on the left !
+function updateAnswerPanel(symbolList, romajiList, currentIndex) {
+  document.getElementById("last-symbol").innerHTML = symbolList[currentIndex];
+  document.getElementById("last-transcription").innerHTML =
+    romajiList[currentIndex];
+}
+
+// update counter of errors / success
+function UpdateCounter() {
+  document.querySelector("#correct-counter").innerHTML = `Correct : ${correct}`;
+  document.querySelector("#bad-counter").innerHTML = `Bad : ${bad}`;
+}
+
+// clears all the div and replaces them with new ones
 function ClearTrainingContext(symbolContainerList) {
   for (symbol of symbolContainerList) {
     symbol.remove();
   }
 }
 
+// loads the data needed for trying from a JSON file /content/nameofthejson.json
 async function LoadTrainingData(type) {
   let json = null;
   try {
@@ -110,10 +116,11 @@ async function LoadTrainingData(type) {
   return json;
 }
 
+// creates the div for each symbol we load
 function InstanciateSymbol(symbol) {
-  let container = document.getElementById("training-container");
-  let symbolContainer = document.createElement("div");
-  let symbolElement = document.createElement("h1");
+  const container = document.getElementById("training-container");
+  const symbolContainer = document.createElement("div");
+  const symbolElement = document.createElement("h1");
 
   symbolContainer.classList.add("training-symbol-container");
   symbolElement.classList.add("training-symbol");
@@ -124,12 +131,14 @@ function InstanciateSymbol(symbol) {
   return symbolContainer;
 }
 
+// checks if the symbol and the input are the same if true increases success counter if not increases fail counter
 function setSymbolCorrectness(symbolContainer, correctness) {
   return correctness
     ? symbolContainer.classList.add("correct-answer")
     : symbolContainer.classList.add("wrong-answer");
 }
 
+// clears and load new data by calling clearTrainingContext, LoadTrainingData, GenerateTraining and StartListening
 async function clearAndLoadData() {
   container.innerHTML = "Loading symbols...";
   const trainingData = await LoadTrainingData("hiragana");
